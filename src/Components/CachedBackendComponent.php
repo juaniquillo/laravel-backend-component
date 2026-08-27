17ddec6d266c0ced338f54542094c5b664fabd0c
Juaniquillo <juaniquillo@gmail.com>
2026-08-27 14:50:05 -0400

    feat: work on new isCachable concern

---
+++ src/Components/CachedBackendComponent.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Juaniquillo\BackendComponents\Components;
+
+use BackedEnum;
+use Illuminate\Contracts\Support\Htmlable;
+use Juaniquillo\BackendComponents\Concerns\HasContent;
+use Juaniquillo\BackendComponents\Concerns\HasPath;
+use Juaniquillo\BackendComponents\Concerns\HasSettings;
+use Juaniquillo\BackendComponents\Concerns\IsBackendComponent;
+use Juaniquillo\BackendComponents\Concerns\IsCachable;
+use Juaniquillo\BackendComponents\Concerns\IsLivewireComponent;
+use Juaniquillo\BackendComponents\Concerns\IsThemeable;
+use Juaniquillo\BackendComponents\Contracts\AttributeBag;
+use Juaniquillo\BackendComponents\Contracts\BackendComponent;
+use Juaniquillo\BackendComponents\Contracts\CompoundComponent;
+use Juaniquillo\BackendComponents\Contracts\ThemeManager;
+use Juaniquillo\BackendComponents\Themes\DefaultThemeManager;
+
+final class CachedBackendComponent implements CompoundComponent, Htmlable
+{
+    use HasContent,
+        HasPath,
+        HasSettings,
+        IsBackendComponent,
+        IsCachable,
+        IsLivewireComponent,
+        IsThemeable;
+
+    public function __construct(
+        private string|BackedEnum $name,
+        ThemeManager $themeManager = new DefaultThemeManager
+    ) {
+        $this->themeManager = $themeManager;
+    }
+
+    public function getAttributeBag(): AttributeBag
+    {
+        return new DefaultAttributeBag(
+            $this->getAttributes(),
+            $this->processContent(),
+            $this->compileTheme(),
+            $this->getComponentPath(),
+            $this->getSettings(),
+            $this->isLivewire(),
+            $this->getLivewireKey(),
+            $this->getLivewireParams(),
+        );
+    }
+
+    /**
+     * @return array{
+     *  name:  int|string,
+     *  component: class-string<BackendComponent|CompoundComponent>,
+     *  attributes: array<string, int|string|null>,
+     *  contents: array<string,array<string, int|string>|int|string>,
+     *  theme: array{
+     *   manager: class-string<ThemeManager>,
+     *   themes: array<string, array<int|string, string>|string>,
+     *   path: string,
+     *   realPath: string,
+     *  },
+     *  path: string|null,
+     *  settings: array<string, bool|string>,
+     *  isLivewire: bool,
+     *  livewireKey: string|null,
+     *  livewireParams: array<string, mixed>
+     * }
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->getName(),
+            'component' => self::class,
+            'attributes' => $this->getAttributes(),
+            'contents' => $this->processContent()->toArray(),
+            'theme' => [
+                'manager' => \get_class($this->themeManager),
+                'themes' => $this->getThemes(),
+                'path' => $this->themeManager->getDefaultPath(),
+                'realPath' => $this->themeManager->getThemePath(),
+            ],
+            'path' => $this->getPathOnly(),
+            'settings' => $this->getSettings(),
+            'isLivewire' => $this->isLivewire(),
+            'livewireKey' => $this->getLivewireKey(),
+            'livewireParams' => $this->getLivewireParams(),
+        ];
+    }
+
+    /**
+     * Get content as a string of HTML.
+     *
+     * @return string
+     */
+    public function toHtml()
+    {
+        /**
+         * PHPStan bug
+         * https://github.com/larastan/larastan/issues/2213
+         *
+         * @phpstan-ignore argument.type
+         */
+        return \view($this->getContext().'_utilities.resolve-components')
+            ->with('component', $this)
+            ->with('namespace', $this->getContext())
+            ->render();
+    }
+}
